ccb415569297b7fbe5bc0c147aa62f8855499bf7
Grigoriy Popov <grisha_mgn@bk.ru>
2024-07-11 16:44:21 +0500

    approved 94.10

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,6 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import "./App.css";
-
 
 ReactDOM.createRoot(document.getElementById("app")).render(<App />);

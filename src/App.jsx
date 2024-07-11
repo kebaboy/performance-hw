eb63c02b0e59af6b61f0b7305c71659bf7f04cec
Grigoriy Popov <grisha_mgn@bk.ru>
2024-07-11 17:09:47 +0500

    approved 102.39: add footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
+import Footer from "./components/Footer/Footer";
 function App() {
   return (
     <>
       <Header />
       <Main />
+      <Footer />
     </>
   )
 }
